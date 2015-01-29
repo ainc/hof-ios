62d4b6b4d2fffd84486c5cba8867eec89b3de134
Carter Gray <cchgray@gmail.com>
2015-01-29 09:29:10 -0500

    Initial Commit

---
+++ HoFApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HoFApp
+//
+//  Created by Carter Crenshaw Howard Gray on 1/29/15.
+//  Copyright (c) 2015 Carter Crenshaw Howard Gray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
